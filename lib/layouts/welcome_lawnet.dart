c156579e3401105b5cad49b946dc4e346df98b9a
K.D. Sunera Avinash Chandrasiri <kdsuneraavinash@gmail.com>
2018-06-04 09:08:50 +0530

    Removed webview-plugin

--- lib/layouts/welcome_lawnet.dart
+++ lib/layouts/welcome_lawnet.dart
@@ -1,13 +1,10 @@
-import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
-
 import 'package:flutter/material.dart';
 
-class WelcomeLawnet extends StatelessWidget{
+class WelcomeLawnet extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return WebviewScaffold(
-      url: "https://www.lawnet.gov.lk",
+    return Container(
+      color: Colors.lightBlue,
     );
   }
-
 }
